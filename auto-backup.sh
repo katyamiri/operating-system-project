e2901a1709039653ebb7ea5a6bf660215f0cc3d3
katyamiri <www.katiamiri6@gmail.com>
2025-07-19 00:20:15 +0330

    Copied listed files to temporary directory with original structure

--- auto-backup.sh
+++ auto-backup.sh
@@ -6,8 +6,17 @@ FILE_EXT="$2"
 echo "Source directory: $SRC_DIR"
 echo "File extension: $FILE_EXT"
 
+find "$SRC_DIR" -type f -name "*$FILE_EXT" > backup.conf
+
 DEST_DIR="$3"
 mkdir -p "$DEST_DIR"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_NAME="backup_$TIMESTAMP"
+
+TMP_DIR="/tmp/%BACKUP_NAME"
+mkdir "$TMP_DIR"
+
+while read FILE; do
+    cp --parents "$FILE" "$TMP_DIR"
+done < backup.conf
